ae2f17a073a48af4c8bcb4c0e9aaff3c8a013894
Pierre Thibault <pthibault@units.it>
2022-08-03 17:49:37 +0200

    Added documentation on the general principle of the package

--- __init__.py
+++ __init__.py
@@ -1,16 +1,52 @@
 """
 OptImaTo control package
 
-Dependencies (incomplete)
+Terminology
+-----------
+"Device": an instrument with which it is necessary to communicate for motion, detection, etc.
+"Driver": a python object that can be instantiated to manage a device. In practice, a client to the corresponding device server.
+"Device Server": a process that manages and interacts with a given device, through commands sent by one or more clients
+"Socket Device Server": a device server that connects to a device through a TCP socket.
 
-json-database
-click
+General principle
+-----------------
+The design of this software is made to address these limitations:
+- Most devices allow only one connection at a time. It is often useful to access a device through multiple clients,
+  for instance to probe for metadata or specific signals.
+- Keeping logs of a device status requires a process that runs constantly and that keep alive a connection with that device.
+- A crash in a control software might interrupt connections to all devices, requiring a complete reinitialization.
+- Running all drivers in a single software might overload the computer resources
+- Some devices must run on their own machine (Windows), so at the very least these devices need to be "remote controlled".
+
+The solution is to decentralize the device management. Each device has a unique process running a corresponding Device
+Server. Control and data access is done through one or more clients to these servers. Since all communication is through
+TCP sockets, Device Servers can run on different computers, as long as they are on the same network. An "admin" status
+is conferred only to one client at a time to ensure that no two processes attempt at controlling a device
+simultaneously (all "read-only" methods are however allowed by non-admin clients).
+
+In practice, this means that to each device requires two classes, the server (DeviceServer) and the client (Driver). The
+idea is to keep the Device Server as thin as possible, with the "knowledge" related to the Device API implemented
+mostly in the Driver class. This is easily done for all Socket Driver Servers, because then the Device Server simply
+acts as a middleman, or a message broker. Things are more complicated for Driver Servers that interact with the device
+through custom libraries, because the library calls have to be forwarded from the client to the server.
+
+
+Additional classes
+------------------
+The DeviceServer subclasses are meant to be instantiated each on their own process, possibly on their own computer,
+should be running constantly. Users should never need to touch these.
+The Driver subclasses can be instantiated in any python program within the network. To the user, this should be the
+low-level access for specific configuration, experimentation, and debugging.
+Currently, apart from the X-ray source, devices fall in just two main categories: motion devices, and detectors. There
+are therefore two higher level classes (Motor and Camera) that is meant to provide access to the underlying device
+through a common interface. For instance, it might be that capturing a frame on one detector is done through a call of
+the "detect" method, while on the other it is called "record_frame" - but with the Camera wrapper both are called
+"snap". The hope is to make "Motors" and "Camera" instances sufficient for everyday use.
 
 """
 import logging
 import logging.handlers
 import os
-#import optimatools as opt
 from . import util
 from .util import logs, FileDict
 from . import network_conf
@@ -59,12 +95,19 @@ drivers = {}
 # dictionary of motor instances
 motors = {}
 
+# dictionary of camera instances
+cameras = {}
+
 # Import all driver submodules
 from . import aerotech
 from . import mclennan
 from . import mecademic
 from . import microscope
 from . import smaract
+#from . import varex
+#from . import pco
+#from . import xspectrum
+#from . import xps
 
 #from . import mtffun_hans
 #from . import pcofun_hans
