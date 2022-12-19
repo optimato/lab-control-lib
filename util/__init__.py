4c09a7f1293f9746da699987a5b8ccbf068c2ff5
Pierre Thibault <pthibault@units.it>
2022-12-19 09:25:36 +0100

    strange lingering bug in util/__init__.py

--- util/__init__.py
+++ util/__init__.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 from . import logs
 from .filedict import FileDict
-from .datalog import DataLogger
 from .fake_device import FakeDevice
 from .imstream import FramePublisher, FrameSubscriber
 from . import viewers
