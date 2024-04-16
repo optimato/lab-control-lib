43b516735ce3e53ce41ef872b8b89d255751c1dc
Francesco De Carlo <decarlof@gmail.com>
2024-04-16 15:08:42 -0500

    address #2

--- lclib/__init__.py
+++ lclib/__init__.py
@@ -82,6 +82,9 @@ if uname.system == "Linux":
 elif uname.system == "Windows":
     s = subprocess.run(['ipconfig', '/allcompartments'], capture_output=True).stdout.decode()
     local_ip_list = [x.split(' ')[-1] for x in s.split('\r\n') if x.strip().startswith('IPv4')]
+elif uname.system == "Darwin":
+    ip = str(subprocess.check_output(["ifconfig | grep inet"], shell=True)[:-2], 'UTF-8')
+    local_ip_list = [x.split(' ')[1] for x in ip.split('\t') if x.split(' ')[0] == 'inet']
 else:
     raise RuntimeError(f'Unknown system platform {uname.system}')
 
