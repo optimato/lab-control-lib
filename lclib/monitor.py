84d5c091343387b8dc470a92195f879084ee6a6d
Pierre Thibault <pthibault@units.it>
2024-10-01 13:51:24 +0200

    Manager split WIP: removed scan functionality from monitor

--- lclib/monitor.py
+++ lclib/monitor.py
@@ -1,165 +1,93 @@
 """
-Management of experiment data, labeling, metadata, etc.
-
-The structure is inspired from Elettra's storage structure
- - Investigation : highest category (e.g. speckle_long_branch)
- - Experiment : Typically an experiment run (over days, possibly in multiple parts)
- - Scan : (instead of Elettra's "dataset") a numbered (and possibly labeled) dataset
+Management of metadata.
 
 This file is part of lab-control-lib
 (c) 2023-2024 Pierre Thibault (pthibault@units.it)
 """
-import os
-from datetime import datetime
+import logging
 import time
 import threading
 
 from . import (get_config,
-               MANAGER_ADDRESS,
+               MONITOR_ADDRESS,
                _driver_classes,
                client_or_None,
                register_driver,
                proxycall,
                proxydevice)
-from .util import Future, now
+from .util import Future
 from .base import DriverBase
-from .logs import logging_muted
 
-logtags = {'type': 'manager',
-           'branch': 'both'
-           }
-
+# Used to store existing client
 _client = []
 
 
-def getManager():
+def getMonitor():
     """
     A convenience function to return the current client (or a new one) for the Manager daemon.
     """
     if _client and _client[0]:
         return _client[0]
-    d = client_or_None('manager', admin=False, client_name=f'client-{get_config()["this_host"]}')
+    d = client_or_None('monitor', admin=False, client_name=f'client-{get_config()["this_host"]}')
     _client.clear()
     _client.append(d)
     return d
 
 
 @register_driver
-@proxydevice(address=MANAGER_ADDRESS)
-class Manager(DriverBase):
+@proxydevice(address=MONITOR_ADDRESS)
+class Monitor(DriverBase):
     """
-    Management of experiment structures and metadata.
+    Device monitoring and metadata collection.
     """
-
-    # Allowed characters for experiment and investigation names
-    _VALID_CHAR = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-:'
-
-    # Interval at which attempts are made at connecting clients
-    CLIENT_LOOP_INTERVAL = 20.
 
     DEFAULT_CONFIG = DriverBase.DEFAULT_CONFIG.copy()
-    DEFAULT_CONFIG.update(
-                      {'experiment':None,
-                       'investigation':None,
-                       'meta_to_save':{},
-                       'last_scan_info': {}})
 
     def __init__(self):
         """
-        Metadata manager for investigations, experiments and scans.
+        Device supervision and metadata collection.
 
-        An important task of the Manager driver is to request and collect metadata
-        *concurrently* from all other drivers. This is accomplished by creating
-        clients to each driver. These clients are instantiated (and re-instantiated)
-        on a separate thread running `self.client_loop`. For each client, the
-        method `fetch_meta` is started on a separate thread as soon as metadata collection
-        is requested. All this is done to ensure that metadata is obtained
-        as quickly as possible after it has been requested.
+        The Monitor driver keeps clients connected to all available devices, and attempts
+        periodically to connect to those that are known but unavailable.
+        The Monitor can request and collect metadata *concurrently* from all available drivers.
+        See `request_meta` and `return_meta` for an explanation of the metadata collection
+        mechanism.
+
+        The Monitor also has the capability to kill drivers.
+
+        TODO: implement a mechanism to spawn drivers (tricky because multiplatform)
         """
         super().__init__()
-
-        # Set initial parameters
-        self._running = False
-        self._scan_name = None
-        self._label = None
-        self._base_file_name = None
-        self._next_scan = None
-
-        try:
-            self._scan_number = self.next_scan()
-        except Exception as e:
-            self.logger.warning('Could not find first available scan number (have experiment and investigation been set?)')
-        self.counter = 0
-
-        self.metacalls = {'investigation': lambda: self.investigation,
-                          'experiment': lambda: self.experiment,
-                          'last_scan': lambda: self._scan_number or None}
 
         self.requests = {}      # Dictionary to accumulate requests in case many are made before returning
         self.stop_flag = threading.Event()
         self.clients = {}
-        self.scan_info = {}
-        self.last_scan_info = self.config['last_scan_info']
 
-        # HACK (kind of): On the process where this class is instantiated, getManager must return this instance, not a client.
+        # HACK (kind of): On the process where this class is instantiated, getMonitor must return this instance, not a client.
         global _client
         _client.clear()
         _client.append(self)
 
-        # self also instead of "client to self"
-        self.clients['manager'] = self
+        # Create all clients (except to self)
+        self.clients = {name:client_or_None(name, admin=False, client_name='monitor', keep_trying=True) for name in _driver_classes.keys() if name != self.name}
 
-        # Start client monitoring loop
-        self.clients_loop_future = Future(self.clients_loop)
+        # Make clients more quiet
+        for name, c in self.clients.items():
+            c.logger.setLevel(logging.WARNING)
 
-    def clients_loop(self):
-        """
-        A loop running on a thread monitoring the health of the driver connections
-        """
-        while True:
-            # Stop if asked
-            if self.stop_flag.is_set():
-                break
+        # Add self also instead of "client to self"
+        self.clients[self.name] = self
 
-            # Loop through all registered driver classes
-            for name in _driver_classes.keys():
-                if name.lower() == self.name.lower():
-                    continue
-                # If client does not exist
-                if name not in self.clients:
-                    # Attempt client instantiation
-                    with logging_muted():
-                        client = client_or_None(name, admin=False, client_name='manager_loop')
-                    if client:
-                        # Successful client connection
-                        self.logger.info(f'Client "{name}" is connected')
-
-                        # Store client
-                        self.clients[name] = client
-                else:
-                    try:
-                        cl = self.clients[name]
-                        cl.conn.ping()                        
-                    except (EOFError, TimeoutError) as error:
-                        # Client is dead for some reason. We clean this up and restart it
-                        self.logger.warning(f'Closing client to {name} because of failed ping: {repr(error)}')
-                        cl = self.clients.pop(name)
-                        try:
-                            cl.disconnect()
-                        except:
-                            pass
-            # Wait a bit before retrying
-            if self.stop_flag.wait(self.CLIENT_LOOP_INTERVAL):
-                break
-        self.logger.info('Exiting client connection loop.')
+        # This is used for stats
+        self.connected = True
 
     def fetch_meta(self, name):
         """
         Method run on a short-lived thread just the time to fetch metadata.
         """
         client = self.clients.get(name)
-        if client is None:
-            self.logger.warning(f'Client {name} not present.')
+        if client is None or not client.connected:
+            self.logger.warning(f'Client {name}: no metadata available.')
             return None
         t0 = time.time()
         meta = client.get_meta()
@@ -168,14 +96,16 @@ class Manager(DriverBase):
         return {'meta':meta, 'time': dt}
 
     @proxycall()
-    def request_meta(self, request_ID=None, exclude_list=[]):
+    def request_meta(self, request_ID=None, include_list=None, exclude_list=None):
         """
         Request metadata from all connected clients.
 
-        This method returns immediately. The metadata itself will be obtained when calling return_meta.
+        This method starts one thread (`Future` per client) and returns immediately. The metadata itself will be
+        obtained when calling return_meta.
 
         Args:
             request_ID: a (hopefully unique) ID to tag and store the request until self.return_meta is called. It can be None.
+            include_list: ignored if None. Otherwise, request metadata only of the components in include_list.
             exclude_list: a list of clients to exclude for the metadata requests.
         Returns:
             None
@@ -185,8 +115,12 @@ class Manager(DriverBase):
         if duplicate is not None:
             self.logger.warning(f'Requests ID {request_ID} has not been claimed and will be overwritten.')
 
+        exclude_list = exclude_list or []
+        if include_list is None:
+            include_list = [name for name in self.clients.keys() if name not in exclude_list]
+
         # Fetch metadata on separate threads
-        self.requests[request_ID] = {name:Future(self.fetch_meta, (name,)) for name in self.clients.keys() if name not in exclude_list}
+        self.requests[request_ID] = {name:Future(self.fetch_meta, (name,)) for name in include_list}
         return
 
     @proxycall()
@@ -208,6 +142,7 @@ class Manager(DriverBase):
         if not request:
             self.logger.warning(f'Empty request: {request_ID}!')
 
+        # Grab all available metadata
         meta = {}
         times = {}
         for name, future in request.items():
@@ -216,19 +151,30 @@ class Manager(DriverBase):
             else:
                 result = future.result()
                 if result is not None:
+                    # TODO: some diagnostics using the times dictionary
                     meta[name] = result['meta']
                     times[name] = result['time']
 
         return meta
 
     @proxycall(admin=True)
-    def killall(self):
+    def killall(self, components=None):
         """
         Kill all servers - except self!
+
+        Args:
+            components: if not None, kill only listed components. Default is None - kill all.
         """
-        while self.clients:
-            name, c = self.clients.popitem()
-            if name == 'manager':
+        if components is None:
+            components = self.clients.keys()
+
+        for name in components:
+            try:
+                c = self.clients.pop(name)
+            except KeyError:
+                self.logger.error(f'Unknown component {name}!')
+                continue
+            if name == self.name:
                 # We don't kill ourselves
                 continue
             c.ask_admin(True, True)
@@ -241,104 +187,20 @@ class Manager(DriverBase):
         Clean up
         """
         self.stop_flag.set()
-        m =  getManager()
+        m =  getMonitor()
         if m:
             del m
-        self.clients_loop_future.join()
-
-    @proxycall()
-    def start_scan(self, label=None):
-        """
-        Start a new scan.
-        Args:
-            label: an optional label to be used for directory and file naming.
-
-        Returns:
-            `scan_info` dict with scan information.
-        """
-        if self._running:
-            raise RuntimeError(f'Scan {self.scan_name} already running')
-
-        # New scan start time
-        start_time = now()
-
-        # Get new scan number
-        self._scan_number = self.next_scan()
-
-        # Create scan name
-        today = datetime.now().strftime('%y-%m-%d')
-
-        scan_name = f'{self._scan_number:06d}_{today}'
-        if label is not None:
-            scan_name += f'_{label}'
-
-        self._scan_name = scan_name
-        self._base_file_name = scan_name + '_{0:06d}'
-
-        self._running = True
-        self._label = label
-        self.counter = 0
-
-        # Create path (ok even if on control host)
-        os.makedirs(os.path.join(get_config()['data_path'], self.path, scan_name), exist_ok=True)
-
-        scan_info = {'scan_number': self._scan_number,
-                'scan_name': scan_name,
-                'investigation': self.investigation,
-                'experiment': self.experiment,
-                'path': self.path,
-                'start_time': start_time}
-
-        # Store for status access
-        self.scan_info = scan_info
-
-        return scan_info
-
-    @proxycall()
-    def end_scan(self):
-        """
-        Finalize the scan
-        """
-        if not self._running:
-            raise RuntimeError(f'No scan currently running')
-
-        self._running = False
-
-        self.last_scan_info = self.scan_info
-        self.last_scan_info['end_time'] = now()
-        self.last_scan_info['counter'] = self.counter
-
-        self.config['last_scan_info'] = self.last_scan_info
-
-        self.scan_info = {}
-
-        return self.last_scan_info
 
     @proxycall()
     def status(self):
         """
-        Summary of current configuration as a string
+        Current status of the system.
         """
-        s = f' * Investigation: {self.investigation}\n'
-        s += f' * Experiment: {self.experiment}\n'
-        ns = self.next_scan()
-        s += f' * Last scan number: {"[none]" if (ns is None or ns==0) else ns-1}'
-        return s
-
-    @proxycall()
-    def scan_status(self):
-        """
-        Return information about current scan if running, otherwise about the last scan
-        """
-        out = {}
-        if self._running:
-            out.update(self.scan_info)
-            out['counter'] = self.counter
-            out['now'] = now()
-        else:
-            out.update(self.last_scan_info)
-
-        return out
+        # Number of connected clients
+        Ntotal = len(self.clients)
+        Nconnected = len([c for n, c in self.clients.items() if c.connected])
+        stats = self.get_stats()
+        return {'clients': Ntotal, 'connected': Nconnected, 'stats': stats}
 
     @proxycall()
     def get_stats(self):
@@ -348,6 +210,8 @@ class Manager(DriverBase):
         stats = {}
         for name, c in self.clients.items():
             try:
+                if not c.connected:
+                    continue
                 raw_stats = c.stats
             except AttributeError:
                 # c could be self
@@ -370,145 +234,3 @@ class Manager(DriverBase):
                             'N': N}
             stats[name] = client_stats
         return stats
-
-    @proxycall()
-    def next_prefix(self):
-        """
-        Return full prefix identifier and increment counter.
-        """
-        if not self._running:
-            raise RuntimeError(f'No scan currently running')
-        prefix = self._base_file_name.format(self.counter)
-        self.counter += 1
-        return prefix
-
-    @proxycall()
-    def get_counter(self):
-        """
-        Return current counter value (unlike next_prefix, does not increment the counter)
-        """
-        return self.counter
-
-    @proxycall()
-    def next_scan(self):
-        """
-        Return the next available scan number based on the analysis of the
-        experiment path.
-        """
-        try:
-            exp_path = os.path.join(get_config()['data_path'], self.path)
-        except RuntimeError as e:
-            return None
-        scan_numbers = [int(f.name[:6]) for f in os.scandir(exp_path) if f.is_dir()]
-        return max(scan_numbers, default=-1) + 1
-
-    def _check_path(self):
-        """
-        If the current investigation / experiment values are set, check if path exists.
-        """
-        try:
-            full_path = os.path.join(get_config()['data_path'], self.path)
-            if os.path.exists(full_path):
-                self.logger.info(f'Path {full_path} selected (exists).')
-            else:
-                os.makedirs(full_path, exist_ok=True)
-                self.logger.info(f'Created path {full_path}.')
-        except RuntimeError as e:
-            self.logger.warning(str(e))
-
-    def _valid_name(self, s):
-        """
-        Confirm that the given string can be used as part of a path
-        """
-        return all(c in self._VALID_CHAR for c in s)
-
-    @proxycall()
-    @property
-    def scan_name(self):
-        """
-        Return the full scan name - none if no scan is running.
-        """
-        if not self._running:
-            return None
-        return self._scan_name
-
-    @proxycall()
-    @property
-    def investigation(self):
-        """
-        The current investigation name.
-
-        *** Setting the investigation makes experiment None. ***
-        """
-        return self.config.get('investigation')
-
-    @investigation.setter
-    def investigation(self, v):
-        if v is None:
-            raise RuntimeError(f'Investigation should not be set to "None"')
-        if self._running:
-            raise RuntimeError(f'Investigation cannot be modified while a scan is running.')
-        if not self._valid_name(v):
-            raise RuntimeError(f'Invalid investigation name: {v}')
-        self.config['investigation'] = v
-        self.config['experiment'] = None
-
-    @proxycall()
-    @property
-    def experiment(self):
-        """
-        The current experiment name.
-        """
-        return self.config['experiment']
-
-    @experiment.setter
-    def experiment(self, v):
-        if v is None:
-            raise RuntimeError(f'Experiment should not be set to "None"')
-        if self._running:
-            raise RuntimeError(f'Experiment cannot be modified while a scan is running.')
-        if self.investigation is None:
-            raise RuntimeError(f'Investigation is not set.')
-        if not self._valid_name(v):
-            raise RuntimeError(f'Invalid experiment name: {v}')
-        self.config['experiment'] = v
-        self._check_path()
-
-    @proxycall()
-    @property
-    def scanning(self):
-        """
-        True if a scan is currently running
-        """
-        return self._running
-
-    @property
-    def path(self):
-        """
-        Return experiment path
-        """
-        experiment = self.experiment
-        investigation = self.investigation
-        if (experiment is None) or (investigation is None):
-            raise RuntimeError('Experiment or Investigation not set.')
-        return os.path.join(investigation, experiment)
-
-    @proxycall()
-    @property
-    def scan_path(self):
-        """
-        Return scan path - None if no scan is running.
-        """
-        if not self._running:
-            return None
-        return os.path.join(self.path, self.scan_name)
-
-    @proxycall()
-    @property
-    def scan_number(self):
-        """
-        Return scan name - None if no scan is running.
-        """
-        if not self._running:
-            return None
-        return self._scan_number
